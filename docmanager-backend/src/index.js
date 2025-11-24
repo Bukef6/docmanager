6cc2e78514e8339a3593ece4eb86f96db827034a
Katarína Marcinová <katamarcina@gmail.com>
2025-11-24 22:17:10 +0100

    bug fs

--- docmanager-backend/src/index.js
+++ docmanager-backend/src/index.js
@@ -3,6 +3,7 @@ const express = require("express");
 const cors = require("cors");
 const cookieParser = require("cookie-parser");
 const path = require("path");
+const fs = require("fs");
 
 const authRoutes = require("./routes/authRoutes");
 const documentRoutes = require("./routes/documentRoutes");
